070f72277e44cf7dc327cdffdcb340fbadda7665
Andasali Abdykalykov <trueandas@gmail.com>
2020-10-31 22:31:13 +0600

    Prepared project

--- app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
@@ -7,62 +7,11 @@ import android.util.Log
 import android.widget.Button
 import kz.kolesateam.confapp.hello.presentation.HelloActivity
 
-private const val TAG = "MainActivity"
-
 class MainActivity : AppCompatActivity() {
-
-    private val openHelloButton: Button by lazy {
-        findViewById(R.id.activity_main_open_hello_button)
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        openHelloButton.setOnClickListener {
-            val helloScreenIntent = Intent(this, HelloActivity::class.java)
-            startActivity(helloScreenIntent)
-        }
-
-        Log.d(TAG, "onCreate")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-
-        Log.d(TAG, "Inside onRestart")
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        Log.d(TAG, "Inside onStart")
-
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        Log.d(TAG, "onPause")
-
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d(TAG, "onStop")
-
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "onDestroy")
-
-        super.onDestroy()
     }
 }
