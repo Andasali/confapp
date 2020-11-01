14f9a56fe836997315766fcba7fcdc03bc1d51a0
Andasali Abdykalykov <trueandas@gmail.com>
2020-11-01 11:54:31 +0600

    Added backend

--- app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
@@ -1,17 +1,70 @@
 package kz.kolesateam.confapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import kz.kolesateam.confapp.hello.presentation.HelloActivity
 
+const val USER_NAME_KEY = "user_name"
+const val APPLICATION_SHARED_PREFERENCES = "application"
+
 class MainActivity : AppCompatActivity() {
+
+    private val openHelloButton: Button by lazy {
+        findViewById(R.id.activity_main_continue_button)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (isUserSaved()){
+            navigateToHelloScreen()
+            finish()
+        }
+
+        setContentView(R.layout.activity_main)
+
+        val nameEditText: EditText = findViewById(R.id.activity_main_editText_name)
+
+        openHelloButton.setOnClickListener {
+            saveUserName(nameEditText.text.toString())
+            navigateToHelloScreen()
+        }
+
+
+
+
+    }
+
+    private fun isUserSaved() : Boolean  {
+        val sharedPreferences: SharedPreferences = getSharedPreferences(
+                APPLICATION_SHARED_PREFERENCES,
+                Context.MODE_PRIVATE
+        )
+
+        return sharedPreferences.contains(USER_NAME_KEY)
+    }
+
+    private fun saveUserName(userName: String) {
+        val sharedPreferences: SharedPreferences = getSharedPreferences(
+                APPLICATION_SHARED_PREFERENCES,
+                Context.MODE_PRIVATE
+        )
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+        editor.putString(USER_NAME_KEY, userName)
+        editor.apply()
+    }
+
+    private fun navigateToHelloScreen() {
+        val helloScreenIntent = Intent(this, HelloActivity::class.java)
+        startActivity(helloScreenIntent)
     }
 }
--- app/src/main/java/kz/kolesateam/confapp/hello/presentation/HelloActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/hello/presentation/HelloActivity.kt
@@ -1,10 +1,15 @@
 package kz.kolesateam.confapp.hello.presentation
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import kz.kolesateam.confapp.APPLICATION_SHARED_PREFERENCES
 import kz.kolesateam.confapp.R
+import kz.kolesateam.confapp.USER_NAME_KEY
 
 class HelloActivity : AppCompatActivity() {
 
@@ -13,5 +18,21 @@ class HelloActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello)
 
+        val nameText: TextView = findViewById(R.id.activity_hello_name_text)
+        val userName: String = getSavedUserName()
+
+        nameText.text = resources.getString(R.string.welcom, userName)
+
     }
+
+
+    private fun getSavedUserName(): String {
+        val sharedPreferences: SharedPreferences = getSharedPreferences(
+                APPLICATION_SHARED_PREFERENCES,
+                Context.MODE_PRIVATE
+        )
+
+        return sharedPreferences.getString(USER_NAME_KEY, "Hi") ?: "Hi"
+    }
+
 }
