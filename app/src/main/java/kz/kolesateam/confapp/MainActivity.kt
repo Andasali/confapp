c4f863b91a2aa358441b472d1e578ec2acb0a4d1
Andasali Abdykalykov <trueandas@gmail.com>
2020-11-02 14:53:25 +0600

    Changed project structure and code

--- app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
@@ -7,69 +7,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import kz.kolesateam.confapp.hello.presentation.HelloActivity
-
-const val USER_NAME_KEY = "user_name"
-const val APPLICATION_SHARED_PREFERENCES = "application"
+import kz.kolesateam.confapp.hello.presentation.TestHelloActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private val openHelloButton: Button by lazy {
-        findViewById(R.id.activity_main_continue_button)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
 
         setContentView(R.layout.activity_main)
 
-        val nameEditText: EditText = findViewById(R.id.activity_main_editText_name)
+        navigateToHelloScreen()
 
-        openHelloButton.setOnClickListener {
-            saveUserName(nameEditText.text.toString())
-            navigateToHelloScreen()
-        }
-
-        nameEditText.addTextChangedListener(object : TextWatcher {
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-                if (!p0.isNullOrBlank() && !p0.isNullOrEmpty()) {
-                    openHelloButton.setBackgroundColor(0xFF2582DE.toInt())
-                    openHelloButton.setTextColor(0xFFFFFFFF.toInt())
-                    openHelloButton.isEnabled = true
-                }
-                else if(p0.isNullOrBlank() && p0.isNullOrEmpty()){
-                    openHelloButton.setBackgroundColor(0xFFE9E8E9.toInt())
-                    openHelloButton.setTextColor(0xFF797777.toInt())
-                    openHelloButton.isEnabled = false
-                }
-            }
-
-            override fun afterTextChanged(p0: Editable?) {}
-        })
-
-
-    }
-
-
-    private fun saveUserName(userName: String) {
-        val sharedPreferences: SharedPreferences = getSharedPreferences(
-                APPLICATION_SHARED_PREFERENCES,
-                Context.MODE_PRIVATE
-        )
-        val editor: SharedPreferences.Editor = sharedPreferences.edit()
-
-        editor.putString(USER_NAME_KEY, userName)
-        editor.apply()
     }
 
     private fun navigateToHelloScreen() {
--- app/src/main/java/kz/kolesateam/confapp/hello/presentation/HelloActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/hello/presentation/HelloActivity.kt
@@ -1,38 +1,65 @@
 package kz.kolesateam.confapp.hello.presentation
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
-import android.widget.TextView
-import kz.kolesateam.confapp.APPLICATION_SHARED_PREFERENCES
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import kz.kolesateam.confapp.R
-import kz.kolesateam.confapp.USER_NAME_KEY
+
+const val USER_NAME_KEY = "user_name"
+const val APPLICATION_SHARED_PREFERENCES = "application"
 
 class HelloActivity : AppCompatActivity() {
 
+    private val openHelloButton: Button by lazy {
+        findViewById(R.id.activity_hello_continue_button)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_hello)
 
-        val nameText: TextView = findViewById(R.id.activity_hello_name_text)
-        val userName: String = getSavedUserName()
+        val nameEditText: EditText = findViewById(R.id.activity_hello_editText_name)
 
-        nameText.text = resources.getString(R.string.welcom_fmt, userName)
+        openHelloButton.setOnClickListener {
+            saveUserName(nameEditText.text.toString())
+            navigateToHelloScreen()
+        }
 
+        nameEditText.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(str: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+            override fun onTextChanged(str: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+            override fun afterTextChanged(str: Editable?) {
+                val isInputEmpty: Boolean = str.toString().isBlank()
+                openHelloButton.isEnabled = !isInputEmpty
+            }
+        })
     }
 
 
-    private fun getSavedUserName(): String {
+    private fun saveUserName(userName: String) {
         val sharedPreferences: SharedPreferences = getSharedPreferences(
                 APPLICATION_SHARED_PREFERENCES,
                 Context.MODE_PRIVATE
         )
 
-        return sharedPreferences.getString(USER_NAME_KEY, "Hi") ?: "Hi"
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+        editor.putString(USER_NAME_KEY, userName)
+        editor.apply()
     }
 
+    private fun navigateToHelloScreen() {
+        val helloScreenIntent = Intent(this, TestHelloActivity::class.java)
+        startActivity(helloScreenIntent)
+    }
 }
