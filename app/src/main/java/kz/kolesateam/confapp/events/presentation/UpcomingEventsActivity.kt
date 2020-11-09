dd7d906d28a6cbe664004620e265ea1fbac4af7b
Andasali Abdykalykov <trueandas@gmail.com>
2020-11-09 10:42:20 +0600

    Created Design

---
+++ app/src/main/java/kz/kolesateam/confapp/events/presentation/UpcomingEventsActivity.kt
@@ -0,0 +1,12 @@
+package kz.kolesateam.confapp.events.presentation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UpcomingEventsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
